5d8271f04434fdb2e779910be1b48162f2525e99
courseocp7 <courseocp7@gmail.com>
2016-10-19 14:22:17 +0200

    Modified Singleton example

--- ocp/src/main/java/javacourse/ocp/objectdesingprinciples/patterns/Singleton.java
+++ ocp/src/main/java/javacourse/ocp/objectdesingprinciples/patterns/Singleton.java
@@ -61,9 +61,11 @@ class Singleton02 {
 
 	public static Singleton02 getInstance() {
 
-		synchronized (Singleton.class) {
-			if (instance == null) {
-				instance = new Singleton02();
+		if (instance == null) { 
+			synchronized (Singleton.class) {
+				if (instance == null) { // double checking...
+					instance = new Singleton02();
+				}
 			}
 		}
 
@@ -79,3 +81,44 @@ class Singleton02 {
 
 	}
 }
+
+
+enum SingletonAsEnum {
+	
+	SINGLETON(0);
+	
+	{
+		System.out.println("init block...");
+	}
+	
+	private int num;
+	
+	private SingletonAsEnum(int num) {
+		this.num = num;
+	}
+	
+	public void setNum(int num){
+		this.num = num;
+	}
+	
+	void doSomething(){
+		System.out.printf("doing something from Singleton instance %d", this.num);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		SingletonAsEnum instance = SingletonAsEnum.SINGLETON;
+		SingletonAsEnum instance2 = SingletonAsEnum.SINGLETON;
+		
+		
+		System.out.println(instance == instance2);
+		
+		instance.setNum(50);
+		instance2.doSomething();
+		
+	}
+	
+}
+
+
