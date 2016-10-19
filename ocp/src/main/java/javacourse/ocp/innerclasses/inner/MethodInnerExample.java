17f4faa6bbd2dbe4d3f202ddd0543c216f02ee97
courseocp7 <courseocp7@gmail.com>
2016-10-19 14:34:20 +0200

    Added inner method class example

---
+++ ocp/src/main/java/javacourse/ocp/innerclasses/inner/MethodInnerExample.java
@@ -0,0 +1,26 @@
+package javacourse.ocp.innerclasses.inner;
+
+public class MethodInnerExample {
+	
+	private int num = 50;
+
+	public void doSomething() {
+		final int num = 23;
+
+		class Inner { // method inner class
+			public void print() {
+				System.out.println("Inside method inner class " + MethodInnerExample.this.num); //  we can access private from outer...
+				System.out.println("Inside method inner class " + num); //  we can't access non-final enclosing var...
+			}
+		} // end of inner class
+
+		// Accessing the inner class
+		Inner inner = new Inner();
+		inner.print();
+	}
+
+	public static void main(String args[]) {
+		MethodInnerExample outer = new MethodInnerExample();
+		outer.doSomething();
+	}
+}
